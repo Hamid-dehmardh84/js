964d84d260a76fe46a69310df5c1c8683e2674a6
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-05-21 15:54:09 +0430

    js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,37 @@
-const hamid = [1,2,3,4,]
-hamid.push (5);
-console.log(hamid);
+
+
+
+
+
+
+
+// localStorage.setItem('name', 'ali sheihk');
+// console.log(localStorage.getItem('name'));
+const posts = [{ id: 1, name: 'post 1'}, {id: 2, name: 'post 2'}];
+localStorage.setItem('post', JSON.stringify());
+
+
+
+
+
+
+document.getElementById('omid').innerHTML="hamid webprog io";
+
+
+
+
+// localStorage.setItem('posts', JSON.stringify(posts));
+// console.log(localStorage.getItem('name'));
+
+
+// const posts = ['post 1','post'];
+// localStorage.setItem('posts', JSON.stringify(posts));
+// console.log(JSON.stringify(posts))
+
+
+// sessionStorage.setItem
+
+// console.log(localStorage)
+// const hamid = [1,2,3,4,]
+// hamid.push (5);
+// console.log(hamid);
