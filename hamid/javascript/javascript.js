8b5478c90d53d695a6db3c2cc65578a8eedd9b67
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-07-06 23:06:29 +0430

    javascript

---
+++ hamid/javascript/javascript.js
@@ -0,0 +1,17 @@
+ 
+ let age = 25; 
+
+   let name = 'ali'
+
+   const person = {
+    name:'liam',
+    age:20,
+   }
+   let newperson = person;
+   newperson.age = 658;
+
+   
+   let newName = name;
+   newName = 'sara'
+   console.log(person)
+   console.log(age)
