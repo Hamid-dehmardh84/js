629970f652a72b459fa26b374ac585ce401647a9
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-07-10 10:54:24 +0430

    javascript

---
+++ source/js/app.js
@@ -0,0 +1,106 @@
+
+const b = new String( document.getElementById('div-a-p-buck').innerHTML="hamid webprog io");
+//  console.log(b)
+//  console.log(b[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let age = 25; 
+//    let name = 'ali'
+//    const person = {
+//     name:'liam',
+//     age:20,
+//    }
+//    let newperson = person;
+//    newperson.age = 658;
+//    let newName = name;
+//    newName = 'sara'
+//    console.log(person)
+//    console.log(age)
+// x = 'hello' + 'world';
+// const hamid = 45;
+// x = 1; 
+// x = x+5
+//  let x;  
+//  x = 5 + '15';  
+// console.log(x ,typeof x );  
+
+
+// const firstName = 'abdol';
+
+// const lastName = 'hamid';
+
+// let x = 'my name iz ' + firstName + ' ' + lastName;
+
+//  x = `my name iz ${firstName} ${lastName}`;
+
+//  console.log(x)
+
+
+//  const ali =  'ali sakrami';
+//  const hamid = 'abdol hamid';
+//  let y = 'my name iz '  +  ali + ' ' + hamid ;
+//  y =  `my name iz ${ali} ${hamid}`
+
+// y = new String(`my name iz  ${ali}  ${2+2} ${hamid}`);
+//  console.log(y);
+//  console.log(y[0]);
+// //  const s = "webprog..io";
+//  const s = new String("webprog..io");
+//  console.log(s);
+
+//  x = s.length;
+//  console.log(s[0]);
+
+// const b = new String( document.getElementById('div-a-p-buck').innerHTML="hamid webprog io");
+// d = b.length;
+// d = b.charAt();
+// d = b.toUpperCase( );
+// // d = b.substring(1 , 6 );
+// console.log(d);
+
+
+// k =  Math.floor(Math.random( ) * 20 +1 ) ;
+
+// k =  Math.floor( * 10)
+// console.log(k)
+
+// let username 
+ 
+
+
+
+// const mmadi = [6500] ; 
+// H = mmadi.push(500);
+// console.log(mmadi)
+
+
+// let username = "ali akbari - 02";
+
+// console.log(username)
+// console.log(username)
+// console.log(username)
+// console.log(username)
+
+// let userage = 19
+// console.log(userage)
+
+// const price = 9000
+// console.log(price)
+let age = 25;
+console.log(age);
+console.log(typeof age);
+let StringAge = String(age);
+console.log(StringAge);
+console.log(typeof StringAge);
