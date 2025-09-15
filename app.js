69c7f221e41f34446f8b5428a3dc4112d0eca018
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-09-16 00:44:30 +0430

    javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+person = {
+    name:'liam',
+}
+console.log(person)
+let name = 'ali';
+let newName = name ;
+newName ='hamid';
+console.log(newName)
+ 
+let newPerson =  person;
+newPerson.name='nima';
+console.log(newPerson)
