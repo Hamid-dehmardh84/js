88763c53a176c24a7b71da5d7293ae74db1f208c
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-09-19 02:58:12 +0430

    js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const sara = new String("webprog.io");
-console.log(sara)
-x = sara.length;
+const  arr = [1, 2, 3, 4, 5];
+// arr.push(10)
+// arr.pop()
+// arr.unshift(10)
+// arr.shift();
+// arr.reverse()
+// x = arr.includes(3);
+// x = arr.indexOf(3)
+x = arr.splice( 2, 100);
 
+console.log(arr , x);
