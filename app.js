5449b30315285517cf47b7de799a83592ecc1edc
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-10-07 11:58:58 +0330

    javascript

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-const  arr = [1, 2, 3, 4, 5];
-// arr.push(10)
-// arr.pop()
-// arr.unshift(10)
-// arr.shift();
-// arr.reverse()
-// x = arr.includes(3);
-// x = arr.indexOf(3)
-x = arr.splice( 2, 100);
-
-console.log(arr , x);
+  function  sayHellow() {
+    console.log('hellow hamid');
+  }
+sayHellow()
