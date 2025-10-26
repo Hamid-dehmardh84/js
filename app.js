d1c37536bb13b6e1ebb713bc3e44f62fcc6ce38c
Hamid-dehmardh84 <hdhmrdh30@gmail.com>
2025-10-26 19:20:25 +0330

    js

--- app.js
+++ app.js
@@ -1,131 +1,16 @@
 
 
+function myFunction() {
+ let on = document.getElementById('demo');
+  on.style.fontSize = "25px";
+  on.style.color = "red";
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if(false) {
-//    console.log('hamid');
-// }
-
-// let convert = '1.9';
-// convert =  parseFloat(convert);
-// let x = 5; 
-// let y = 5;
-// let omid  = {
-//     name:'hello',
-//     age: 352,
-//     taxe:{
-//       name1:'hello from hamid',
-//     },
-// };
-// x =  omid;
-
-// x = 'foo' / 89 ; 
-
-
-
-// const obj1 = {
-//     id:1,
-//     title:'Todo 1',
-//     user:{
-//         name:'ali',
-//     },
-// };
-
-// if(x < y) {
-//     console.log(`${x} < ${y}`);
-// }else{
-//     console.log(' یافت نشد ')
-// }
-
- 
-//  const ducumentb =  document.getElementById('button-js-1').innerHTML="hamid webprog io";
-//  let x;
-//  if (open:'hamid', LastNam') = 
-//     // LastName:'dehmardeh',
-//  }
-// console.log(ducumentb);
-
-// // const { id: todoID, title, user: { name } } = obj1;
-
-
-
-// const x = {
-//      obj1,
-//      obj2,
-// }
-//  x = Object.hasOwnProperty();
-
-// console.log(todoID , title , name);
-// const y = 1;
-
-
-// if (x > y) {
-//     console.log(`${x} > ${y}`);
-// };
-
-
-// if(x < y) {
-//     console.log(`${x} < ${y}`);
-// }else{{
-    // open:'hamid',
-//     console.log(' یافت نشد ')
-// }
-
-// if (x === y) {
-//     console.log(`${x} === ${y}`);
-// };
-
-
-// const l = 
-
-
-
-
-
-
-//  const alih =   (6 , 6) => x - h ;
-//      console.log(alih)
-
-
-
-
-
-
-
-// function addo (hamid , labobo) {
-//     return hamid + labobo;
-//     // const ik = 15;
-//     // const ki = 20;
-//     // return ik + ki ;
-
-// }
-// console.log(30+20)
-
-// console.log(addo(8 , 10));
-
-
-// const mehdi = [ 12, 'ali h', 25 ];
-//  const nameh  = {
-//     parsa: 'hamid' ,
-//     age: 33 ,
-//  };
-//  console.log(nameh)
-//  console.log(mehdi)
-// const add2 =    
-
+  let x = 0;
+   o = 20;
+  if  (x  > o ){
+   console.log(o);
+  }else{
+   alert(myFunction()) , console.log("noting");
+  };
